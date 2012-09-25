a2a513eb08dc67639f54bf0f7ee51061f7f9b982
Tiago Babo <tiagobabo@wifi-interno.fe.up.pt>
2012-09-25 19:26:15 +0100

    Adds rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicQuiz::Application.config.secret_token = '9a0756938b292b7cf817f2269241d8aecb69593544ef23d5981531b402309e8299b65491d83414180629d4bc7369b96b32960864e99aacbd4e45d88c1cfad324'
